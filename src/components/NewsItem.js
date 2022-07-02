b3b46c834cedcdcb22c671b38252e444ca900559
Mohit jukariya <Jukariyamj@gmail.com>
2022-07-03 00:29:33 +0530

    react router added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
         <div className="card mx-2 my-2">
           <img src={imageUrl ? imageUrl : "https://images.livemint.com/img/2022/06/25/600x338/415be322-4602-11eb-bc1d-4bfe13e32b0e_1608858841050_1608858879986_1656146286557.jpg"} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}  <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex: '1', left: '90%'}}>
+            <h5 className="card-title">{title}  <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex: '1', left: '90%'}}>
               {source}
             </span></h5>
             <p className="card-text">{description}</p>
